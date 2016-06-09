4051ee4ad0fa5e326956ead4f7d225fe7b9d4520
iSach <sacha.lewin@me.com>
2016-06-09 22:55:40 +0200

    Minor Changes

--- src/main/java/be/isach/samaritan/command/CommandCat.java
+++ src/main/java/be/isach/samaritan/command/CommandCat.java
@@ -32,7 +32,7 @@ class CommandCat extends Command {
      */
     @Override
     void onExecute(String[] args) {
-        getMessageChannel().sendMessage("HEEEY");
+        getMessageChannel().sendMessage("HEEEY CA MARCHE");
         getMessageChannel().sendMessage(getSamaritan().getGifFactory().getRandomGif("cat"));
     }
 }
