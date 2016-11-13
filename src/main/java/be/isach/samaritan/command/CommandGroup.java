cc6ba6a373bd23727adaaaa07088f6d4426c9880
iSach <sacha.lewin@me.com>
2016-11-13 23:44:08 +0100

    Fixed pages of help

--- src/main/java/be/isach/samaritan/command/CommandGroup.java
+++ src/main/java/be/isach/samaritan/command/CommandGroup.java
@@ -184,13 +184,15 @@ public class CommandGroup extends Command {
 
         getMessageChannel().sendMessage(indexOfLangs + "");
 
+        int i = 0;
         for(Role role : getGuild().getRoles()) {
-            if(role.getPosition() > indexOfLangs
-                    || role.getName().contains("everyone")) {
-                continue;
-            }
+            //if(role.getPosition() > indexOfLangs
+            //        || role.getName().contains("everyone")) {
+            //   continue;
+            //}
 
-            stringBuilder.append("  ").append(role.getName().toLowerCase()).append("\n");
+            stringBuilder.append("  ").append(role.getName().toLowerCase()).append("   ").append(i).append("\n");
+            i++;
         }
         stringBuilder.append("```");
         getMessageChannel().sendMessage(stringBuilder.toString());
